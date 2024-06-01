bc2ba2d2ffb5bcc63b4d846e26da362987feffec
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-01 17:59:23 -0400

    Remove files pertaining to CheckoutPage component

--- src/components/CheckoutPage.jsx
+++
@@ -1,9 +0,0 @@
-function CheckoutPage() {
-  return (
-    <>
-      <h2>Checkout Page</h2>
-    </>
-  );
-}
-
-export default CheckoutPage;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,5 @@
 import App from "./components/App";
 import CartPage from "./components/CartPage";
-import CheckoutPage from "./components/CheckoutPage";
 import ErrorPage from "./components/ErrorPage";
 import HomePage from "./components/HomePage";
 import ProductPage from "./components/ProductPage";
@@ -16,7 +15,6 @@ const routes = [
       { path: "store", element: <StorePage /> },
       { path: "product/:id", element: <ProductPage /> },
       { path: "cart", element: <CartPage /> },
-      { path: "checkout", element: <CheckoutPage /> },
     ],
   },
 ];
