d9cb91a59d30a51f1b2b4e16bd06a354b6afec58
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-03 19:41:15 -0400

    Re-work App component styles to be modular and non-interfering with other styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
-import "../styles/App.module.css";
+import styles from "../styles/App.module.css";
 import { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState(new Map());
 
+  const menuItems = [
+    { to: "/", label: "Home" },
+    { to: "store", label: "Store" },
+    { to: "cart", label: `Cart (${cart.size})` },
+  ];
+
   return (
     <>
       <nav>
         <menu>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="store">Store</Link>
-          </li>
-          <li>
-            <Link to="cart">Cart ({cart.size})</Link>
-          </li>
+          {menuItems.map((item) => (
+            <li key={item.to} className={styles["menu-item"]}>
+              <Link to={item.to}>{item.label}</Link>
+            </li>
+          ))}
         </menu>
       </nav>
       <Outlet context={[cart, setCart]} />
