832ff8f9295313abc2f07ab16a1baddcce4db660
Varulli <120419874+Varulli@users.noreply.github.com>
2024-06-03 22:25:47 -0400

    Change the cart page from rows of flex boxes to a table

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { priceFormatter } from "../utils";
+import styles from "../styles/CartItem.module.css";
 
 function CartItem({ product, handleRemove }) {
   return (
-    <li className="cart-item">
-      <img src={product.image} alt={product.title} />
-      <Link to={`/product/${product.id}`} state={{ product }}>
-        {product.title}
-      </Link>
-      <p>{product.price}</p>
-      <p>{product.quantity}</p>
-      <button onClick={handleRemove}>Remove</button>
-    </li>
+    <tr className={styles["cart-item"]}>
+      <td>
+        <img src={product.image} alt={product.title} />
+      </td>
+      <td>
+        <Link to={`/product/${product.id}`} state={{ product }}>
+          {product.title}
+        </Link>
+      </td>
+      <td>{priceFormatter.format(product.price)}</td>
+      <td>{product.quantity}</td>
+      <td>
+        <button className={styles["remove"]} onClick={handleRemove}>
+          Remove
+        </button>
+      </td>
+    </tr>
   );
 }
 
--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,5 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import CartItem from "./CartItem";
+import styles from "../styles/CartPage.module.css";
+import { priceFormatter } from "../utils";
 
 function CartPage() {
   const [cart, setCart] = useOutletContext();
@@ -8,17 +10,43 @@ function CartPage() {
     <main>
       <h2>Cart</h2>
 
-      <ul>
-        {Array.from(cart.values()).map((product) => (
-          <CartItem
-            key={product.id}
-            product={product}
-            handleRemove={() =>
-              setCart(new Map([...cart].filter(([id]) => id !== product.id)))
-            }
-          />
-        ))}
-      </ul>
+      <table className={styles["cart-items"]}>
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Title</th>
+            <th scope="col">Price</th>
+            <th scope="col">Quantity</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(cart.values()).map((product) => (
+            <CartItem
+              key={product.id}
+              product={product}
+              handleRemove={() =>
+                setCart(new Map([...cart].filter(([id]) => id !== product.id)))
+              }
+            />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th scope="row" colSpan="2">
+              Total
+            </th>
+            <td colSpan="3">
+              {priceFormatter.format(
+                Array.from(cart.values()).reduce(
+                  (total, product) => total + product.price * product.quantity,
+                  0
+                )
+              )}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
 
       <button type="button" disabled={cart.size === 0}>
         Checkout
